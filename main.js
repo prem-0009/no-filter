20daddf44ec9d7fc0f3d946aabf22de300e14f43
prem <prem.sherpa@codeimmersives.com>
2020-07-07 12:25:34 -0400

    more of for-of

--- main.js
+++ main.js
@@ -1,37 +1,80 @@
 /******************
  * YOUR CODE HERE *
  ******************/
+const onlyOdds = function (arr) {
+  let result = [];
+  for (const num of arr) {
+    if (num % 2 !== 0) {
+      result.push(num);
+    }
+  }
 
+  return result;
+};
 
+//2
+//even no
+const onlyEvens = function (arr) {
+  let result = [];
+  for (const num of arr) {
+    if (num % 2 === 0) {
+      result.push(num);
+    }
+  }
 
+  return result;
+};
 
+//3
+//names with less than 7 char
+const shortNamesOnly = function (arrOfNames) {
+  let result = [];
+  for (const names of arrOfNames) {
+    if (names.length < 7) {
+      result.push(names);
+    }
+  }
 
+  return result;
+};
+
+//4
+//names starting with d or D
+const dNames = function (arrOfNames) {
+  let result = [];
+  for (const name of arrOfNames) {
+    if (name[0] === "d" || name[0] === "D") {
+      result.push(name);
+    }
+  }
+
+  return result;
+};
+// dNames(["dena", "hamp", "Demmy"]);
 
 /*********************************
  * OUR CODE BELOW; DO NOT TOUCH! *
  *********************************/
 
-
-if (typeof onlyOdds === 'undefined') {
+if (typeof onlyOdds === "undefined") {
   onlyOdds = undefined;
 }
 
-if (typeof onlyEvens === 'undefined') {
+if (typeof onlyEvens === "undefined") {
   onlyEvens = undefined;
 }
 
-if (typeof shortNamesOnly === 'undefined') {
+if (typeof shortNamesOnly === "undefined") {
   shortNamesOnly = undefined;
 }
 
-if (typeof dNames === 'undefined') {
+if (typeof dNames === "undefined") {
   dNames = undefined;
 }
-
 
 module.exports = {
   onlyOdds,
   onlyEvens,
   shortNamesOnly,
   dNames,
-}
+};
